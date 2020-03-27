94b68cf947f0b14a7d93aa884a822bb6ae027368
Cesar Alberto Nava Chavez <ing.cesarchaveznava@gmail.com>
2020-03-27 14:04:37 -0600

    Registration dates in the profiles

--- functions/src/infrastructure/services/authentication/authentication.service.ts
+++ functions/src/infrastructure/services/authentication/authentication.service.ts
@@ -1,13 +1,16 @@
 import { CreateProfileAuthPort } from "../../../application/usecases/authentication/create-profile-auth.usecase";
-import { WriteBatch } from "@google-cloud/firestore";
+import { WriteBatch, FieldValue } from "@google-cloud/firestore";
 import { db } from "../../../config/app";
 
 export class AuthenticationService implements CreateProfileAuthPort {
     async createProfile(uid: string, email: string): Promise<void> {
         const batch: WriteBatch = db.batch();
+        const date = FieldValue.serverTimestamp();
 
         batch.create(db.collection('profiles').doc(uid), {
-            email: email
+            email: email,
+            firstUpdate: date, 
+            lastUpdate: date
         });
 
         await batch.commit();
