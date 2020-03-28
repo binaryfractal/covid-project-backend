a606ee35c7b1f2d9ba784554c808cf0470e75e3c
Cesar Alberto Nava Chavez <ing.cesarchaveznava@gmail.com>
2020-03-28 15:25:37 -0600

    Post in profile controller for saving

--- functions/src/infrastructure/controllers/profile/profile.controller.ts
+++ functions/src/infrastructure/controllers/profile/profile.controller.ts
@@ -7,19 +7,24 @@ import {
    // Body,
     Res, 
     HttpStatus, 
-    Param
+    Param,
+    Post,
+    Body
 } from "@nestjs/common";
 import { FindOneProfile } from "../../../application/usecases/profile/find-one-profile.usecase";
 import { Profile } from "../../../domain/models/profile";
 import { ProfileService } from "../../../infrastructure/services/profile/profile.service";
+import { SaveProfile } from "../../../application/usecases/profile/save-profile.usecase";
 //import { UserRecord } from "firebase-functions/lib/providers/auth";
 
 @Controller("profiles")
 export class ProfileController {
     private readonly findOneProfileUsecase: FindOneProfile<ProfileService>;
+    private readonly saveProfileUsecase: SaveProfile<ProfileService>;
 
     constructor() {
         this.findOneProfileUsecase = new FindOneProfile(new ProfileService());
+        this.saveProfileUsecase = new SaveProfile(new ProfileService());
     } 
 
     @Get(':uid')
@@ -30,6 +35,20 @@ export class ProfileController {
             // const profile: Profile = await this.findOneProfileUsecase.findOne(user.uid); 
             const profile: Profile = await this.findOneProfileUsecase.findOne(uid);
             res.status(HttpStatus.OK).send(profile);
+        } catch(error) {
+            res.status(HttpStatus.INTERNAL_SERVER_ERROR).send({ message: error.message });
+        }
+    }
+
+    @Post(':uid')
+    async save(@Body('profile') profile: Profile, @Param('uid') uid: string, @Res() res: Response): Promise<void> {
+    //@Post()
+    //async save(@Body('profile') profile: Profile, @Body('user') user: UserRecord, @Res() res: Response): Promise<void> {
+        try {
+            //profile.uid = user.uid;
+            profile.uid = uid;
+            await this.saveProfileUsecase.save(profile);
+            res.status(HttpStatus.CREATED).send(true);
         } catch(error) {
             res.status(HttpStatus.INTERNAL_SERVER_ERROR).send({ message: error.message });
         }
